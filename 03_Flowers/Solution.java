955064dc146b507c498630a55583b5ebe5ee204e
mahtal_m <mahtal_m@etna-alterance.net>
2023-12-06 11:05:43 +0100

    exo3

---
+++ 03_Flowers/Solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public static void main(String[] args) {
+        int[] flowerbed = {1,0,0,0,1};
+        int n = 1;
+
+        Boolean result = canPlaceFlowers( flowerbed,  n);
+        System.out.println("Result 1: " + result);
+    }
+    
+    private static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        Boolean result= true; 
+        
+        return result;
+    }
+        
+
+}
